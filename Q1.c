2943b2874c8feed8b1d0312d5473b93b379621b8
Lydia He <75390984+iydia@users.noreply.github.com>
2024-02-29 10:47:58 -0500

    limited runtime

--- Q1.c
+++ Q1.c
@@ -12,20 +12,19 @@ double min(double a, double b) {
 int q1a_helper(double prev_closest_approx, int q) {
 
     double closest_this_round = 1;
-    double bestp = 1;
+    double bestp = 0;
 
     for (double thisp = q; thisp <= 2*q; ++thisp) {
-        if (fabs((double)(thisp/q - sqrt(2))) < fabs((double)(closest_this_round - sqrt(2)))) {
-            closest_this_round = (double)thisp/q;
-            bestp = thisp;
+        double p_by_q = (double)thisp/q;
+        if (fabs((double)(p_by_q - sqrt(2))) < fabs((double)(closest_this_round - sqrt(2)))) {
+            if (fabs((double)(p_by_q - sqrt(2))) < fabs((double)(prev_closest_approx - sqrt(2)))) {
+                closest_this_round = (double)p_by_q;
+                bestp = thisp;
+            }
         }
     }
 
-    if (closest_this_round < prev_closest_approx) {
-        return bestp;
-    } else {
-        return 0;
-    }
+    return bestp;
 }
 
 // Prints each term of the sequence for (a)
@@ -35,18 +34,26 @@ void print_q1a(int p, int q, double closest_approx) {
 }
 
 void q1a() {
+
+    int terms_found = 0;
     int p = 1;
     double closest_approx = 1;
 
+    // begin with p=1, q=1
     print_q1a(1, 1, closest_approx);
 
-    for (int q = 2; q <= 100; ++q) {
+    int q = 2;
 
+    while (terms_found <= 20) {
         p = q1a_helper(closest_approx, q);
 
-        closest_approx = fabs((double)p/q);
+        if (p > 0) {
+            closest_approx = fabs((double)p/q);
+            print_q1a(p, q, closest_approx);
+            ++terms_found;
+        }
 
-        print_q1a(p, q, closest_approx);
+        ++q;
     }
 }
 
